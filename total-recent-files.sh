7a4c1ee61d39e1db503ec52f35b35404f8689852
U-OFFICE\rreijmers <rreijmers@SUPLT04.office.denit.net>
2013-08-06 21:02:03 +0200

    initial test

--- total-recent-files.sh
+++ total-recent-files.sh
@@ -1,2 +1,17 @@
 #!/bin/bash
 
+path=$1
+days=$2
+
+# find all files modified x days ago
+files=`find ${path} -mtime -${days} -type f`
+
+# Set variable
+byteCount=0;
+
+for file in ${files[*]};
+do
+		bytes=`du ${file} | awk '{print $1}'`;
+		let "byteCount=${byteCount} + ${bytes}";
+done;
+
